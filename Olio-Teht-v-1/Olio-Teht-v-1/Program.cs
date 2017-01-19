63b81f9635ac41d8c27ca09ce846177ae80ec95e
h8324 <h8324@OHJE-330-WS-29>
2017-01-19 11:07:44 +0200

    Initial commit.

---
+++ Olio-Teht-v-1/Olio-Teht-v-1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio_Teht_v_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku;
+            Console.Write("Anna luku > ");
+            luku = int.Parse(Console.ReadLine());
+            Console.Write("Annoit luvun ");
+            if (luku == 1)
+                Console.WriteLine("yksi");
+            else if (luku == 2)
+                Console.WriteLine("kaksi");
+            else if (luku == 3)
+                Console.WriteLine("kolme");
+            else
+                Console.WriteLine("Et antanut lukua");
+            Console.ReadLine();
+        }
+    }
+}
